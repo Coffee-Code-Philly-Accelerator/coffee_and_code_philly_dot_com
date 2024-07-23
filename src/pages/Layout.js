8a2f42a9bbf0d537e9524906e1a30280518da9c4
derekmeegan <derek.j.meegan@gmail.com>
2024-07-23 18:28:45 -0400

    update footer to be simple

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Footer from "components/footers/FiveColumnWithInputForm.js";
+import Footer from "components/footers/MiniCenteredFooter.js";
 
 const Layout = () => {
   return (
