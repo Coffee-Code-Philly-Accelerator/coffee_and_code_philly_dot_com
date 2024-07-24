5ed568d1514680e9dd79df1de7641b810c9962f6
MosesSupposes <mosesasamuel633@gmail.com>
2024-07-24 16:53:23 -0400

    remove comment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-// App.js
-
 import React from "react";
 import "style.css";
 import "tailwindcss/lib/css/preflight.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AboutUs from "./pages/AboutUs";
 import ContactUs from "pages/ContactUs";
-import Projects from "pages/Projects";
 import Layout from "pages/Layout";
 import Home from "pages/Home";
 import OnboardingQuiz from "components/onboarding_quiz/OnboardingQuiz";
+import BlogIndex from "pages/BlogIndex";
+import Events from "pages/Events"; // Import the Events component
 
 const App = () => {
   const router = createBrowserRouter([
@@ -19,15 +18,10 @@ const App = () => {
       children: [
         { path: "/", element: <Home /> },
         { path: "/about", element: <AboutUs /> },
-<<<<<<< HEAD
-        { path: "/projects", element: <Projects /> },
-        { path: "/joinUs", element: <ContactUs /> },
-=======
         { path: "/projects", element: <BlogIndex /> },
         { path: "/events", element: <Events /> },
         { path: "/contact", element: <ContactUs /> },
         { path: "/join-us", element: <OnboardingQuiz /> },
->>>>>>> eea14a0 (WIP implement form validation on onboarding quiz)
       ],
     },
   ]);
