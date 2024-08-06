c84ad6db29e310c2ddae0b10e76c1557296e8729
MosesSupposes <mosesasamuel633@gmail.com>
2024-08-06 19:57:44 -0400

    style the quiz with better spacing; rephrase one of the questions; eliminate remaining bugs and dead comments

--- src/components/onboarding_quiz/QuizItem.js
+++ src/components/onboarding_quiz/QuizItem.js
@@ -6,11 +6,12 @@ import React, { useState } from "react";
  *
  * @param {Object} props - The component props.
  * @param {string} props.question - The question to be displayed.
+ * @param {string} props.isFinalQuestion - A flag to indicate if the question is the final question.
  * @param {function} props.onNextQuestion - The callback function to be called when the next question button is clicked.
  * @param {Array<string>} props.choices - The array of choices for the question.
  * @returns {JSX.Element} The rendered quiz item component.
  */
-function QuizItem({ question, onNextQuestion, choices }) {
+function QuizItem({ question, isFinalQuestion, onNextQuestion, choices }) {
   const [selectedChoice, setSelectedChoice] = useState(null);
 
   const handleChange = (event) => {
@@ -21,13 +22,26 @@ function QuizItem({ question, onNextQuestion, choices }) {
   const uniqueName = `onboarding_quiz_${question.replace(/\s+/g, "_")}`;
 
   return (
-    <div>
+    <div
+      style={{
+        marginBottom: "15px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
       <fieldset>
         <legend>
           <i>{question}</i>
         </legend>
         {choices.map((choice, index) => (
-          <React.Fragment key={index}>
+          <div
+            key={index}
+            style={{
+              display: "block",
+              margin: "5px",
+            }}
+          >
             <input
               type="radio"
               id={`${uniqueName}_${choice}`}
@@ -38,7 +52,7 @@ function QuizItem({ question, onNextQuestion, choices }) {
               checked={selectedChoice === choice}
             />
             <label
-              key={index}
+              key={index * 10}
               htmlFor={`${uniqueName}_${choice}`}
               style={{
                 paddingLeft: "4px",
@@ -47,7 +61,7 @@ function QuizItem({ question, onNextQuestion, choices }) {
             >
               {choice}
             </label>
-          </React.Fragment>
+          </div>
         ))}
       </fieldset>
       <button
@@ -60,12 +74,17 @@ function QuizItem({ question, onNextQuestion, choices }) {
           cursor: "pointer",
           marginTop: "5px",
           fontWeight: "bold",
+          display: "block",
+          justifyContent: "center",
+          margin: "0 auto",
         }}
+        disabled={selectedChoice === null}
         onClick={(event) => {
           // Prevent the form from being submitted
           event.preventDefault();
           if (selectedChoice !== null) {
             onNextQuestion(selectedChoice);
+            setSelectedChoice(null);
           }
         }}
       >
